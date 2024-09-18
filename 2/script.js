55fb5af9eeb63c2175ced6b58b7b04ea90c88b6c
LeoFolino <leonelfolinoing@gmail.com>
2024-09-18 18:00:30 -0300

    Repaso Objetos y Arrays

---
+++ 2/script.js
@@ -0,0 +1,31 @@
+const array = ['HTML','CSS','JavaScript','ReactJS']
+
+const object = {
+    nombre: 'LeoFolino',
+    ocupacion: 'DevOps',
+    materia: 'JavaScript para DOM'
+}
+
+const objectArray = [ //array de objetos
+    {
+    materia: 'HTML', 
+    dificultad: 6,
+    necesario: true
+    },
+    {
+    materia: 'CSS', 
+    dificultad: 8,
+    necesario: true
+    },
+    {
+    materia: 'JavaScript', 
+    dificultad: 9.5,
+    necesario: false
+    },
+    {
+    materia: 'ReactJS', 
+    dificultad: 8,
+    necesario: false
+    }
+    
+]
