2ef71abad1e00a37c4bf68f969a531f080e4a9d8
LeoFolino <leonelfolinoing@gmail.com>
2024-09-18 19:10:12 -0300

    Reorganizacion de carpetas

---
+++ 3-DOM/script.js
@@ -0,0 +1,5 @@
+// document.getElementById('test').innerHTML = 'Hola Mundo desde JS' // esto selecciona 1 solo elemento y por eso es modificable
+
+// document.getElementsByClassName // esto selecciona varios elementos
+
+document.getElementById('test').setAttribute('class','colorado')
