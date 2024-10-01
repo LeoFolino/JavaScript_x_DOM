39c73bb1744d95d408031b79ea6921cabb18e34e
LeoFolino <leonelfolinoing@gmail.com>
2024-10-01 20:15:18 -0300

    primerformacion Manipular Nodos

---
+++ 7-Manipular_Nodos/script.js
@@ -0,0 +1,10 @@
+// creando un nodo de parrafo
+const paragraph = document.createElement('p')
+// aca creamos un elemento de texto
+const node = document.createTextNode('Este es el contenido nuevo')
+// aca incorporamos el elemento de texto al nodo de parrafo
+paragraph.appendChild(node)
+// en la variable container tenemos el div
+const container = document.getElementById('div1')
+const p1 = document.getElementById('p1')
+container.appendChild(paragraph)
