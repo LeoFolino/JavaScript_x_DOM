1e13da394e927b7ae21012b0adc245ebe4915216
LeoFolino <leonelfolinoing@gmail.com>
2024-10-08 20:35:01 -0300

    HistoryAPI

---
+++ Modulo2/Introduccion_APIs/HistoryAPI/script.js
@@ -0,0 +1,4 @@
+const goBackx2 = () => window.history.go(-2) // permite ir 2 hacia atras
+const goBack = () => window.history.back // uno hacia atras
+const goForward = () => window.history.forward // uno hacia adelante
+const goForwardx2 = () => window.history.go(+2) // dos hacia adelante
