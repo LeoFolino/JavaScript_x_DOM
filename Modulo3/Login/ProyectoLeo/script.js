9d2dfbb18e487b15774cd16b41fa9bd661dbd9b1
LeoFolino <leonelfolinoing@gmail.com>
2024-10-10 00:15:06 -0300

    final proyecto

--- Modulo3/Login/ProyectoLeo/script.js
+++ Modulo3/Login/ProyectoLeo/script.js
@@ -1,35 +1,54 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const loginForm = document.getElementById('loginForm')
-    const emailInput = document.getElementById('emailInput')
-    const passwordInput = document.getElementById('password')
-    const confirmPasswordInput = document.getElementById('confirmPassword')
-    const emailError = document.getElementById('emailError')
-    const passwordError = document.getElementById('passwordError')
-    const confirmPasswordError = document.getElementById('confirmPasswordError')
+document.addEventListener('DOMContentLoaded', function () {
 
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    const confirmPasswordError = document.getElementById('confirmPasswordError');
+    const showHideButton = document.getElementById('show-hide')
 
-    loginForm.addEventListener('submit', function(event){
-        event.preventDefault()
-        // agregar metodo que valida el form
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        validateForm();
     })
 
-    emailInput.addEventListener('blur', function(){
-        // agregar metodo que valide el mail
+    emailInput.addEventListener('blur', function () {
+        validateEmail()
     })
 
-    emailInput.addEventListener('change', function(){
-        // agregar metodo que valide el mail
+    emailInput.addEventListener('change', function () {
+        clearError(emailError)
     })
 
-    passwordInput.addEventListener('change', function(){
-        // agregar metodo que valide el mail
+    passwordInput.addEventListener('change', function () {
+        clearError(passwordError)
     })
 
-    confirmPasswordInput.addEventListener('change', function(){
-        // agregar metodo que valide el mail
+    confirmPasswordInput.addEventListener('change', function () {
+        clearError(confirmPasswordError)
     })
 
-    function validateForm(){
+    showHideButton.addEventListener('click', function () {
+        if (passwordInput.type == 'password') {
+            passwordInput.type = 'text'
+            confirmPasswordInput.type = 'text'
+        } else {
+            passwordInput.type = 'password'
+            confirmPasswordInput.type = 'password'
+        }
+    })
+
+    function validateForm() {
+        const isValidEmail = validateEmail()
+        const isValidPassword = validatePassword()
+        const passwordMatch = validatePasswordMatch()
+
+        if (isValidEmail && isValidPassword && passwordMatch) {
+            saveToLocalStorage()
+            alert('Has ingresado con éxito')
+        }
 
     }
 
@@ -44,6 +63,54 @@ document.addEventListener('DOMContentLoaded', function(){
 
         return true
     }
+
+    function validatePassword() {
+        const passwordValue = passwordInput.value.trim()
+
+        if (passwordValue.length < 6) {
+            showError(passwordError, 'Ingresa una contraseña de al menos 6 caracteres')
+            return false;
+        }
+
+        return true
+    }
+
+    function validatePasswordMatch() {
+        const passwordValue = passwordInput.value.trim();
+        const confirmPasswordValue = confirmPasswordInput.value.trim();
+
+        if (passwordValue != confirmPasswordValue) {
+            showError(confirmPasswordError, 'Las contraseñas no coinciden')
+            return false;
+        }
+
+        return true;
+    }
+
+    function showError(errorElement, message) {
+        errorElement.innerHTML = message;
+        errorElement.style.display = 'block';
+    }
+
+    function clearError(errorElement,) {
+        errorElement.innerHTML = '';
+        errorElement.style.display = 'none';
+    }
+
+    function saveToLocalStorage() {
+        const emailValue = emailInput.value.trim();
+        localStorage.setItem('email', emailValue)
+        const body = bodyBuilderJSON()
+        console.log(body)
+    }
+
+    function bodyBuilderJSON() {
+        return {
+            "email": emailInput.value,
+            "password": passwordInput.value
+        }
+    }
+
+
+
 })
-
-
